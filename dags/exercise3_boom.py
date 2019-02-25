3b20ad0b39c49d3aa9fad00ad95f19b029859f0f
Eduardo da Silva <edasilva@bol.com>
2019-02-25 14:07:16 +0100

    Second exercise - exercise3_boom

---
+++ dags/exercise3_boom.py
@@ -0,0 +1,56 @@
+import airflow
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import BranchPythonOperator, PythonOperator
+
+args = {"owner": "godatadriven", "start_date": airflow.utils.dates.days_ago(14)}
+
+dag = DAG(
+    dag_id="exercise3",
+    default_args=args,
+    description="Demo DAG showing BranchPythonOperator.",
+    schedule_interval="0 0 * * *",
+)
+
+
+def print_weekday(execution_date, **context):
+    print(execution_date.strftime("%a"))
+
+
+print_weekday = PythonOperator(
+    task_id="print_weekday",
+    python_callable=print_weekday,
+    provide_context=True,
+    dag=dag,
+)
+
+# Definition of who is emailed on which weekday
+weekday_person_to_email = {
+    0: "Bob",  # Monday
+    1: "Joe",  # Tuesday
+    2: "Alice",  # Wednesday
+    3: "Joe",  # Thursday
+    4: "Alice",  # Friday
+    5: "Alice",  # Saturday
+    6: "Alice",  # Sunday
+}
+
+
+def print_email(execution_date, **context):
+    return weekday_person_to_email[execution_date.strftime("%u")]
+
+
+email_branching = BranchPythonOperator(
+    task_id="email_branching",
+    dag=dag,
+    python_callable=print_email
+)
+
+
+def all_possible_paths():
+    for weekday in weekday_person_to_email:
+        return DummyOperator(task_id="email_"+weekday_person_to_email.get(weekday), dag=dag)
+
+
+# Create a down stream dependency
+print_weekday >> email_branching >> all_possible_paths() >> DummyOperator(task_id="join", dag=dag)
