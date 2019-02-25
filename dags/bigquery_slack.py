3a3cc5bc71ad370a507c38bfd26a22cf12dadc56
Eduardo da Silva <edasilva@bol.com>
2019-02-25 16:20:43 +0100

    BQ to Slack

--- dags/bigquery_slack.py
+++ dags/bigquery_slack.py
@@ -15,8 +15,8 @@ dag = DAG(
 )
 
 bq_query = "select committer.name, count(*) as number \
-        from bigquery-public-data.github_repos.commits \
-        where date(committer.date) = {{ ds }} \
+        from [bigquery-public-data.github_repos.commits] \
+        where date(committer.date) = `{{ ds }}` \
         group by committer.name \
         order by number asc \
         limit 5"
@@ -32,9 +32,9 @@ def send_to_slack_func(**context):
     operator = SlackAPIPostOperator(
         task_id='send_to_slack',
         text=str(context['return_value']),
-        # todo: should be passed into a variable
+        # todo: should be passed into a variable from Airflow
         token="xoxp-559854890739-559228586160-561116849751-2c717700dd7b7a197765ac21770c9c08",
-        # todo: should be passed into a variable
+        # todo: should be passed into a variable from Airflow
         channel="General"
     )
     return operator.execute(context=context)
