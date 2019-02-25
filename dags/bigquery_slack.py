5619a3c84e53b03b78276cb9666ed64e88dc7bda
Eduardo da Silva <edasilva@bol.com>
2019-02-25 16:07:40 +0100

    BQ to Slack

--- dags/bigquery_slack.py
+++ dags/bigquery_slack.py
@@ -33,7 +33,7 @@ def send_to_slack_func(**context):
         task_id='send_to_slack',
         text=str(context['return_value']),
         # todo: should be passed into a variable
-        token="xoxp-559854890739-559228586160-560304790661-ae28d681f2f1026dd05cfc0a42f27d89",
+        token="xoxp-559854890739-559228586160-561116849751-2c717700dd7b7a197765ac21770c9c08",
         # todo: should be passed into a variable
         channel="General"
     )
