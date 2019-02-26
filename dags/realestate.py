dca57a3c1e4c207ffa49227288935269bba7bb11
Eduardo da Silva <edasilva@bol.com>
2019-02-26 14:31:18 +0100

    realestate: Add dataproc

--- dags/realestate.py
+++ dags/realestate.py
@@ -49,7 +49,7 @@ def upload_file(bucket, filepath):
     hook.upload(
         bucket=bucket,
         object=filepath,
-        filename=os.path.abspath("dags/airflow_training/pyspark/build_statistics.py")
+        filename=os.path.abspath("pyspark/build_statistics.py")
     )
 
 
