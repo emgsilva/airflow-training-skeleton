ca85acf8dd9457879902e4ed572367c84688c38c
Eduardo da Silva <edasilva@bol.com>
2019-02-26 13:58:14 +0100

    realestate: Add http_to_gcs

--- dags/realestate.py
+++ dags/realestate.py
@@ -4,6 +4,11 @@ from airflow.models import Variable
 from airflow.utils.trigger_rule import TriggerRule
 from airflow_training.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
 from airflow_training.operators.http_to_gcs import HttpToGcsOperator
+from airflow.contrib.operators.dataproc_operator import (
+    DataprocClusterCreateOperator,
+    DataprocClusterDeleteOperator,
+    DataProcPySparkOperator,
+)
 
 args = {"owner": "godatadriven", "start_date": airflow.utils.dates.days_ago(4)}
 
@@ -15,7 +20,7 @@ dag = DAG(
 )
 
 pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
-    task_id="pgsl_to_gcs",
+    task_id="get_prices_to_gcs",
     # uses a connection from Airflow - which contains the credentials necessary to access pgsl
     postgres_conn_id="postgres_con",
     sql="SELECT * FROM land_registry_price_paid_uk WHERE transfer_date = '{{ ds }}'",
@@ -25,7 +30,7 @@ pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
 )
 
 http_to_gcs_op = HttpToGcsOperator(
-    task_id="http_to_gcs_op",
+    task_id="get_currency_to_gcs",
     http_conn_id="currency_con",
     endpoint="convert-currency?date={{ds}}&from=GBP&to=EUR",
     gcs_path="currency/{{ds}}/dates.json",
@@ -34,4 +39,35 @@ http_to_gcs_op = HttpToGcsOperator(
     dag=dag,
 )
 
-[pgsl_to_gcs, http_to_gcs_op]
+dataproc_create_cluster = DataprocClusterCreateOperator(
+    task_id="create_dataproc",
+    cluster_name="analyse-pricing-{{ ds }}",
+    project_id="airflowbolcom-fc205e26bebb44fa",
+    num_workers=2,
+    zone="europe-west1-c",
+    dag=dag,
+)
+
+compute_aggregates = DataProcPySparkOperator(
+    task_id='compute_aggregates',
+    # TODO: create operator to upload localfile "build_statistics.py"
+    main='gs://gdd-training/build_statistics.py',
+    cluster_name='analyse-pricing-{{ ds }}',
+    arguments=[
+        "gs://" + Variable.get('gs_bucket') + "/land_registry_price/{{ ds }}/*.json",
+        "gs://" + Variable.get('gs_bucket') + "/currency/{{ ds }}/*.json",
+        "gs://" + Variable.get('gs_bucket') + "/average_prices/{{ ds }}/"
+    ],
+    dag=dag,
+)
+
+dataproc_delete_cluster = DataprocClusterDeleteOperator(
+    task_id="delete_dataproc",
+    cluster_name="analyse-pricing-{{ ds }}",
+    project_id="airflowbolcom-fc205e26bebb44fa",
+    trigger_rule=TriggerRule.ALL_DONE,
+    dag=dag,
+)
+
+[pgsl_to_gcs,
+ http_to_gcs_op] >> dataproc_create_cluster >> compute_aggregates >> dataproc_delete_cluster
