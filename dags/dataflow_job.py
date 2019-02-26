cb712073273c3ffafb4755b400b47c60c3e0550b
Eduardo da Silva <edasilva@bol.com>
2019-02-26 15:20:34 +0100

    realestate: Add dataproc

--- dags/dataflow_job.py
+++ dags/dataflow_job.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 
 import apache_beam as beam
+from airflow.models import Variable
 from apache_beam.examples.cookbook.coders import JsonCoder
 from apache_beam.io import ReadFromText, BigQueryDisposition
 from apache_beam.io import WriteToBigQuery
@@ -16,16 +17,16 @@ def run(argv=None):
     parser.add_argument(
         "--input",
         dest="input",
-        default="gs://airflow-training-data/land_registry_price_paid_uk/*/*.json",
+        default="gs://" + Variable.get('gs_bucket') + "/land_registry_price/{{ ds }}/*.json",
         help="Input file to process.",
     )
     known_args, pipeline_args = parser.parse_known_args(argv)
     pipeline_args.extend(
         [
             "--runner=DataflowRunner",
-            "--project=gdd-airflow-training",
-            "--staging_location=gs://airflow-training-data/dataflow-staging",
-            "--temp_location=gs://airflow-training-data/dataflow-temp",
+            "--project=airflowbolcom-fc205e26bebb44fa",
+            "--staging_location=gs://" + Variable.get('gs_bucket') + "/dataflow-staging",
+            "--temp_location=gs://" + Variable.get('gs_bucket') + "/dataflow-temp",
             "--job_name=gcs-gzcomp-to-bq1",
         ]
     )
@@ -34,30 +35,30 @@ def run(argv=None):
     pipeline_options.view_as(SetupOptions).save_main_session = True
     with beam.Pipeline(options=pipeline_options) as p:
         (
-            p
-            | "ReadFromGCS" >> ReadFromText(known_args.input, coder=JsonCoder())
-            | WriteToBigQuery(
-                "result_table",
-                dataset="result_dataset",
-                project="gdd-airflow-training",
-                schema="city:string, "
-                       "county:string, "
-                       "district:string, "
-                       "duration:string, "
-                       "locality:string, "
-                       "newly_built:boolean, "
-                       "paon:string, "
-                       "postcode:string, "
-                       "ppd_category_type:string, "
-                       "price:numeric, "
-                       "property_type:string, "
-                       "record_status:string, "
-                       "saon:string, "
-                       "street:string, "
-                       "transaction:string, "
-                       "transfer_date:numeric",
-                create_disposition=BigQueryDisposition.CREATE_IF_NEEDED,
-            )
+                p
+                | "ReadFromGCS" >> ReadFromText(known_args.input, coder=JsonCoder())
+                | WriteToBigQuery(
+            "result_table",
+            dataset="result_dataset",
+            project="gdd-airflow-training",
+            schema="city:string, "
+                   "county:string, "
+                   "district:string, "
+                   "duration:string, "
+                   "locality:string, "
+                   "newly_built:boolean, "
+                   "paon:string, "
+                   "postcode:string, "
+                   "ppd_category_type:string, "
+                   "price:numeric, "
+                   "property_type:string, "
+                   "record_status:string, "
+                   "saon:string, "
+                   "street:string, "
+                   "transaction:string, "
+                   "transfer_date:numeric",
+            create_disposition=BigQueryDisposition.CREATE_IF_NEEDED,
+        )
         )
 
 
