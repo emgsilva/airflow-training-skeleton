31bd8fdb82799a043159a20ca7de0d9da5ed0083
Eduardo da Silva <edasilva@bol.com>
2019-02-26 13:37:15 +0100

    realestate: Add http_to_gcs

--- dags/airflow_training/operators/http_to_gcs.py
+++ dags/airflow_training/operators/http_to_gcs.py
@@ -38,15 +38,17 @@ class HttpToGcsOperator(BaseOperator):
 
     def execute(self, context):
         http = HttpHook(self.method, http_conn_id=self.http_conn_id)
+
         self.log.info("Calling HTTP method")
         response = http.run(self.endpoint)
+
         with NamedTemporaryFile() as tmp_file_handle:
             tmp_file_handle.write(response.content)
-        tmp_file_handle.flush()
-        hook = GoogleCloudStorageHook(
-            google_cloud_storage_conn_id=self.gcs_conn_id)
-        hook.upload(
-            bucket=self.gcs_bucket,
-            object=self.gcs_path,
-            filename=tmp_file_handle.name
-        )
+            tmp_file_handle.flush()
+            hook = GoogleCloudStorageHook(
+                google_cloud_storage_conn_id=self.gcs_conn_id)
+            hook.upload(
+                bucket=self.gcs_bucket,
+                object=self.gcs_path,
+                filename=tmp_file_handle.name
+            )
